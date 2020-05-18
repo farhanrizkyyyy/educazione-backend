f4055f434534e7458e216995101b7b88e0a2511f
Farhan Rizky Fauzi Setiawan <56493776+farhanrizkyyyy@users.noreply.github.com>
2020-05-19 05:01:37 +0700

    Add files via upload

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,25 +1,31 @@
-const { users, getAllUsers } = require('../models/users')
+const { users, getAllUsers, getUsersByEmail, getSortUsers } = require('../models/users')
 const qs = require('querystring')
 
 module.exports = {
   getAllUsers: (request, response) => {
-    let { page, limit } = request.query
+    let { page, limit, search, sort, sortBy } = request.query
     page = parseInt(page) || 1
     limit = parseInt(limit) || 5
+    search = search || ''
+    sort = Boolean(parseInt(sort)) || false
+    sortBy = sortBy || 'id'
 
-    const totalData = users.length
+    const totalData = getUsersByEmail(users, search).length
     const totalPage = Math.ceil(totalData / limit)
     const startData = (page && limit && (page * limit) - limit)
     const endData = (page && limit && (page * limit))
 
-    const prevQuery = qs.stringify({
+    const prev = qs.stringify({
       ...request.query,
       ...{ page: page - 1 }
     })
-    const nextQuery = qs.stringify({
+    const next = qs.stringify({
       ...request.query,
       ...{ page: page + 1 }
     })
+
+    const prevQuery = prev
+    const nextQuery = next
 
     const prevLink = `http://localhost:8080/users?${prevQuery}`
     const nextLink = `http://localhost:8080/users?${nextQuery}`
@@ -27,7 +33,7 @@ module.exports = {
     const data = {
       success: true,
       msg: 'list',
-      data: getAllUsers(startData, endData, users),
+      data: getAllUsers(startData, endData, getUsersByEmail(getSortUsers(users, sort, sortBy), search)),
       pageInfo: {
         page,
         perPage: limit,
--- src/models/users.js
+++ src/models/users.js
@@ -1,22 +1,37 @@
 const users = [
   {
     id: 1,
-    email: 'example@mail.co',
+    email: 'admin@mail.co',
     password: '1234'
   },
   {
     id: 2,
-    email: 'example@mail.co',
+    email: 'mod1@mail.co',
     password: '1234'
   },
   {
     id: 3,
-    email: 'example@mail.co',
+    email: 'mod2@mail.co',
     password: '1234'
   },
   {
     id: 4,
-    email: 'example@mail.co',
+    email: 'mod3@mail.co',
+    password: '1234'
+  },
+  {
+    id: 5,
+    email: 'mod4@mail.co',
+    password: '1234'
+  },
+  {
+    id: 6,
+    email: 'mod5@mail.co',
+    password: '1234'
+  },
+  {
+    id: 7,
+    email: 'jajang@mail.co',
     password: '1234'
   }
 ]
@@ -25,5 +40,38 @@ module.exports = {
   users,
   getAllUsers: (start, end, data) => {
     return data.slice(start, end)
+  },
+  getUsersByEmail: (data, email) => {
+    return users.filter(o => o.email.startsWith(email))
+  },
+  getSortUsers: (data, cond, column) => {
+    return data.sort((a, b) => {
+      let column1 = a[column]
+      let column2 = b[column]
+
+      if (typeof a[column] === 'string') {
+        column1 = a[column].toUpperCase()
+      }
+      if (typeof b[column] === 'string') {
+        column2 = b[column].toUpperCase()
+      }
+
+      if (!cond) {
+        if (column1 < column2) {
+          return -1
+        }
+        if (column1 > column2) {
+          return 1
+        }
+      } else {
+        if (column1 > column2) {
+          return -1
+        }
+        if (column1 < column2) {
+          return 1
+        }
+      }
+      return 0
+    })
   }
 }
